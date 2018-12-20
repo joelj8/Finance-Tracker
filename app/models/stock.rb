3a1ed8a8013cd8d22bb288cf0328400d4de5545d
Joel Estrella <joel.j8@gmail.com>
2018-12-20 17:30:06 +0000

    Add Lookup Details and Search Fixed error

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,17 @@
 class Stock < ApplicationRecord
+  def self.new_from_lookup(ticker_symbol)
+    begin
+      looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+      price = strip_commas(looked_up_stock.latest_price)
+      new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: price)
+    rescue Exception => e
+      return nil
+    end
+  end
+  
+  def self.strip_commas(number)
+    # Error al usar el metodo gsub / debo verificar si esto existe en la version de Ruby 
+    # number.gsub(",","")
+    number
+  end 
 end
